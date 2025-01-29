cb4423ed14e725a1829cbdf52d4399a9a8a94bc0
w-kiran <kirannn.js@gmail.com>
2025-01-29 11:31:35 +0545

    v4.0.0

--- backend/index.js
+++ backend/index.js
@@ -7,18 +7,14 @@ import userRoute from './routes/user.route.js'
 import postRoute from "./routes/post.route.js"
 import messageRoute from "./routes/message.route.js"
 import { app,server } from "./socket/socket.js";
+import path from "path";
 
-
-dotenv.config({});
+dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req,res) =>{
-    return res.status(200).json({
-        message:"I'm coming from backend",
-        success: true
-    })
-})
+const __dirname = path.resolve();
+
 
 app.use(express.json());
 app.use(cookieParser());
@@ -34,6 +30,11 @@ app.use(cors(corsOptions));
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/post", postRoute);
 app.use("/api/v1/message", messageRoute);
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+app.get("*",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"));
+})
 
 server.listen(PORT, () =>{
     connectDB();
